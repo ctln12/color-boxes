a614460738b7324e92df2012a2157c95e699f6e4
Cataline Baert <snuf_kin@yahoo.com>
2020-11-12 20:16:07 +0100

    add colors and change definition of boxes

--- src/BoxesContainer.js
+++ src/BoxesContainer.js
@@ -4,12 +4,11 @@ import './BoxesContainer.css';
 
 class BoxesContainer extends Component {
   static defaultProps = {
-    numBoxes: 18,
-    allColors: ["purple", "magenta", "violet", "pink"]
+    allColors: ["#ffba08", "#faa307", "#f48c06", "#e85d04", "#dc2f02", "#d00000", "#9d0208", "#6a040f", "#370617", "#03071e", "#001845", "#002855", "#023e7d", "#0353a4", "#0466c8", "#057af0", "#238ffb", "#4BA3FB"]
   }
   render() {
-    const boxes = Array.from({ length: this.props.numBoxes }).map(
-      () => <Box allColors={this.props.allColors} />
+    const boxes = this.props.allColors.map(
+      (color) => <Box allColors={this.props.allColors} startColor={color} />
     )
     return <div className="BoxesContainer">{boxes}</div>;
   }
